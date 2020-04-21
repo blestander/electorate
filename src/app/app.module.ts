a3c888f353c90c739e488a2148b072e18f80016a
Brendan Lestander <blestander@gmail.com>
2020-04-21 13:55:45 -0400

    Created skeleton auth-required component and auth service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthRequiredComponent } from './auth-required/auth-required.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         FooterComponent,
-        DashboardComponent
+        DashboardComponent,
+        AuthRequiredComponent
     ],
     imports: [
         BrowserModule,
