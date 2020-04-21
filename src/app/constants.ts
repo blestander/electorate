ff75901f06a6e50f3bcad8b74f0e136173fc96ff
Brendan Lestander <blestander@gmail.com>
2020-04-21 15:14:57 -0400

    Adjusted appearance of AuthComponent.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,3 +2,6 @@
 export const DISCORD_AUTH_URL = "https://discordapp.com/api/oauth2/authorize"
 export const DISCORD_SCOPE = "identify%20guilds";
 export const DISCORD_CLIENT_ID = "701436517754863738";
+
+// Token constants
+export const TOKEN_OBTAIN_URL = "";
