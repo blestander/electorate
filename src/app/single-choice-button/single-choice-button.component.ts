f793140fa3fbffde81ac0161a5b2ab450592fbf3
Brendan Lestander <blestander@gmail.com>
2020-05-10 19:48:45 -0400

    Created buttons for each option.

--- src/app/single-choice-button/single-choice-button.component.ts
+++ src/app/single-choice-button/single-choice-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-single-choice-button',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./single-choice-button.component.css']
 })
 export class SingleChoiceButtonComponent implements OnInit {
+
+    @Input() option: string;
 
     constructor() { }
 
