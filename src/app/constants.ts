fdb360c5a8a200cce384d93138b694129e82adfa
Brendan Lestander <blestander@gmail.com>
2020-05-10 16:14:51 -0400

    Setup for listing of polls on dashboard.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,4 +4,4 @@ export const DISCORD_SCOPE = encodeURI("identify guilds");
 export const DISCORD_CLIENT_ID = "701436517754863738";
 
 // Token constants
-export const TOKEN_OBTAIN_URL = "http://localhost:8080/";
+export const TOKEN_OBTAIN_URL = "https://us-central1-electorate-274912.cloudfunctions.net/obtainToken";
