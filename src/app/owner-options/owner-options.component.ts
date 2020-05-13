524275d6de4e907b450e88871dc9af42fa1ef087
Brendan Lestander <blestander@gmail.com>
2020-05-12 21:00:12 -0400

    Owner option buttons now have confirmation dialogs

--- src/app/owner-options/owner-options.component.ts
+++ src/app/owner-options/owner-options.component.ts
@@ -17,4 +17,14 @@ export class OwnerOptionsComponent implements OnInit {
     displayCSS(): string {
         return this.expand ? "flex" : "none";
     }
+
+    finish(): void {
+        window.confirm("Are you sure you want to end this poll?");
+        // TODO
+    }
+
+    kill(): void {
+        window.confirm("Are you sure you want to delete this poll?\nTHIS ACTION IS NOT REVERSIBLE!")
+        // TODO
+    }
 }
