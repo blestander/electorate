87e6bac0691d50f67b48d4757410f1ae4963c089
Brendan Lestander <blestander@gmail.com>
2020-05-14 12:50:40 -0400

    Setup backend prefix for production.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,11 +1,12 @@
+import { environment } from './../environments/environment';
+
 // OAuth 2.0 constants
 export const DISCORD_AUTH_URL = "https://discordapp.com/api/oauth2/authorize"
 export const DISCORD_SCOPE = encodeURI("identify guilds");
 export const DISCORD_CLIENT_ID = "701436517754863738";
 
 // Backend URL constants
-const CLOUD_BACKEND_PREFIX = "https://us-central1-electorate-274912.cloudfunctions.net";
-const BACKEND_PREFIX = "http://localhost:8080"
+const BACKEND_PREFIX = environment.production ? "https://electorate-274912.uk.r.appspot.com" : "http://localhost:8080"
 export const TOKEN_OBTAIN_URL = `${BACKEND_PREFIX}/obtainToken`;
 export const GET_POLL_URL = `${BACKEND_PREFIX}/getPoll`;
 export const CAST_VOTE_URL = `${BACKEND_PREFIX}/castVote`;
