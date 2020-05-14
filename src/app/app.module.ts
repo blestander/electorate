579ab54ad90d037c77e9610b6f0400aeaba518d8
Brendan Lestander <blestander@gmail.com>
2020-05-14 17:05:54 -0400

    Added reactive forms module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,6 +38,7 @@ import { CreateComponent } from './create/create.component';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
+        ReactiveFormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
