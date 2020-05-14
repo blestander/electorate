11a8715e9a3da50a0c5875cd873198f5880954d3
Brendan Lestander <blestander@gmail.com>
2020-05-14 16:50:35 -0400

    Routed CreateComponent to /create.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PollComponent } from './poll/poll.component';
+import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
     {path: '', component: DashboardComponent},
     {path: 'auth', component: AuthComponent},
+    {path: 'create', component: CreateComponent},
     {path: 'poll/:id', component: PollComponent}
 ];
 
