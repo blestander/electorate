10fbcfff039a9e2bfe27775e0c69479f8a67d9fe
Brendan Lestander <blestander@gmail.com>
2020-05-15 08:35:44 -0400

    Inserted RankedChoiceComponent into PollComponent.

--- src/app/ranked-choice/ranked-choice.component.ts
+++ src/app/ranked-choice/ranked-choice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-ranked-choice',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./ranked-choice.component.css']
 })
 export class RankedChoiceComponent implements OnInit {
+
+    @Input() options: string[];
 
     constructor() { }
 
