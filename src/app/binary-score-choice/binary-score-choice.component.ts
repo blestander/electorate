b664464d05997e2f3acb42667cbe554746cae377
Brendan Lestander <blestander@gmail.com>
2020-05-16 15:36:41 -0400

    Integrated choice selection for approval voting.

--- src/app/binary-score-choice/binary-score-choice.component.ts
+++ src/app/binary-score-choice/binary-score-choice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-binary-score-choice',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./binary-score-choice.component.css']
 })
 export class BinaryScoreChoiceComponent implements OnInit {
+
+    @Input() options: string[];
+    @Output() vote = new EventEmitter<string[]>();
 
     constructor() { }
 
