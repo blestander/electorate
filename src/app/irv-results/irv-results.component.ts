a08293a47f83ed1cf60e977eb99d764ae8973861
Brendan Lestander <blestander@gmail.com>
2020-05-16 21:31:58 -0400

    Refactored IRV results into own component.

--- src/app/irv-results/irv-results.component.ts
+++ src/app/irv-results/irv-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-irv-results',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IRVResultsComponent implements OnInit {
 
+    @Input() results;
+
+    stage: number;
+
     constructor() { }
 
     ngOnInit(): void {
+        this.stage = this.results.length - 1;
     }
 
+    stageClass(index) {
+        if (index == this.stage)
+            return "irv-stage-selected irv-stage";
+        return "irv-stage";
+    }
+
+    stageName(index) {
+        if (this.results.length == 1 || index + 1 == this.results.length)
+            return "Final";
+        else if (index == 0)
+            return "Initial";
+        else
+            return `Stage ${index + 1}`;
+    }
+
+    setStage(index) {
+        this.stage = index;
+    }
 }
