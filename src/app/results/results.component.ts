2e986149dbd0424a12f0f9df0d4ad0a8f4e166b8
Brendan Lestander <blestander@gmail.com>
2020-05-17 07:56:53 -0400

    Purged extraneous IRV functions from ResultsComponent

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -11,58 +11,7 @@ export class ResultsComponent implements OnInit {
     @Input() method: string;
     @Input() options: string[];
 
-    stageIRV: number;
-
     constructor() { }
 
-    ngOnInit(): void {
-        if (this.method == "irv") {
-            this.stageIRV = this.results.length - 1;
-        }
-    }
-
-    votes(option) {
-        switch (this.method) {
-            case "fptp":
-            case "approval":
-                return this.results[option];
-            case "irv":
-                return this.results[this.stageIRV][option];
-        }
-    }
-
-    stageNameIRV(index) {
-        if (this.results.length == 1 || index + 1 == this.results.length)
-            return "Final";
-        else if (index == 0)
-            return "Initial";
-        else
-            return `Stage ${index + 1}`;
-    }
-
-    classIRV(index) {
-        if (index == this.stageIRV)
-            return "irv-stage-selected irv-stage";
-        return "irv-stage";
-    }
-
-    sortedIRVResults() {
-        let stage = this.results[this.stageIRV];
-
-        let sortedOptions = Object.keys(stage).sort((a, b) => {
-            return stage[b] - stage[a];
-        });
-
-        let groupedOptions = new Array();
-        let previous = Number.MAX_SAFE_INTEGER;
-        sortedOptions.forEach((value, index) => {
-            if (stage[value] == previous)
-                groupedOptions[groupedOptions.length - 1].push(value);
-            else {
-                previous = stage[value];
-                groupedOptions.push([value]);
-            }
-        });
-        return groupedOptions;
-    }
+    ngOnInit(): void { }
 }
