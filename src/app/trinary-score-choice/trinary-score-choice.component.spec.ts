d602d66102ff37d0bf7710da27c962a19a1ba184
Brendan Lestander <blestander@gmail.com>
2020-05-17 18:53:33 -0400

    Corrected spacing.

--- src/app/trinary-score-choice/trinary-score-choice.component.spec.ts
+++ src/app/trinary-score-choice/trinary-score-choice.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TrinaryScoreChoiceComponent } from './trinary-score-choice.component';
 
 describe('TrinaryScoreChoiceComponent', () => {
-  let component: TrinaryScoreChoiceComponent;
-  let fixture: ComponentFixture<TrinaryScoreChoiceComponent>;
+    let component: TrinaryScoreChoiceComponent;
+    let fixture: ComponentFixture<TrinaryScoreChoiceComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TrinaryScoreChoiceComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ TrinaryScoreChoiceComponent ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TrinaryScoreChoiceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TrinaryScoreChoiceComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/trinary-score-choice/trinary-score-choice.component.ts
+++ src/app/trinary-score-choice/trinary-score-choice.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-trinary-score-choice',
-  templateUrl: './trinary-score-choice.component.html',
-  styleUrls: ['./trinary-score-choice.component.css']
+    selector: 'app-trinary-score-choice',
+    templateUrl: './trinary-score-choice.component.html',
+    styleUrls: ['./trinary-score-choice.component.css']
 })
 export class TrinaryScoreChoiceComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
