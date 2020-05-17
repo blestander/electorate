af14759c6c0bf0fbce0e6e5d634660ca2d917648
Brendan Lestander <blestander@gmail.com>
2020-05-17 08:32:44 -0400

    Smith table now populated.

--- src/app/smith-results/smith-results.component.ts
+++ src/app/smith-results/smith-results.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SmithResultsComponent implements OnInit {
 
     @Input() smith;
+    @Input() options;
 
     constructor() { }
 
