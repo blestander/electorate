83cabbcfb91469d4a12fe1a5188aa9b4c4def4e7
Brendan Lestander <blestander@gmail.com>
2020-05-18 17:48:29 -0400

    Changed backend URL prefix to electorate.blestander.com

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,7 +6,7 @@ export const DISCORD_SCOPE = encodeURI("identify guilds");
 export const DISCORD_CLIENT_ID = "701436517754863738";
 
 // Backend URL constants
-const BACKEND_PREFIX = environment.production ? "https://electorate-274912.uk.r.appspot.com" : "http://localhost:8080"
+const BACKEND_PREFIX = environment.production ? "https://electorate.blestander.com" : "http://localhost:8080"
 export const TOKEN_OBTAIN_URL = `${BACKEND_PREFIX}/obtainToken`;
 export const GET_POLL_URL = `${BACKEND_PREFIX}/getPoll`;
 export const CAST_VOTE_URL = `${BACKEND_PREFIX}/castVote`;
