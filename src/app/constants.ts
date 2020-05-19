dfd5d660d0d30e43ebc034f2a66287ebe665ea98
Brendan Lestander <blestander@gmail.com>
2020-05-19 07:53:27 -0400

    TOKEN_OBTAIN_URL updated.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -7,7 +7,7 @@ export const DISCORD_CLIENT_ID = "701436517754863738";
 
 // Backend URL constants
 const BACKEND_PREFIX = environment.production ? "https://electorate.blestander.com/api" : "http://localhost:8080/api"
-export const TOKEN_OBTAIN_URL = `${BACKEND_PREFIX}/obtainToken`;
+export const TOKEN_OBTAIN_URL = `${BACKEND_PREFIX}/login`;
 export const GET_POLL_URL = `${BACKEND_PREFIX}/getPoll`;
 export const CAST_VOTE_URL = `${BACKEND_PREFIX}/castVote`;
 export const FINISH_POLL_URL = `${BACKEND_PREFIX}/finishPoll`;
