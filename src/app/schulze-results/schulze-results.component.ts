1d4929e1ff4a562c00fd14b7ab18a3cefbf932b8
Brendan Lestander <blestander@gmail.com>
2020-05-24 17:43:39 -0400

    Incorporated Schulze results into results page.

--- src/app/schulze-results/schulze-results.component.ts
+++ src/app/schulze-results/schulze-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-schulze-results',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./schulze-results.component.css']
 })
 export class SchulzeResultsComponent implements OnInit {
+
+    @Input() results;
 
     constructor() { }
 
