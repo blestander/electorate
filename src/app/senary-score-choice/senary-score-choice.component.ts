7fdb5e4089b2cf344bd86ca6b86576d754f0e9e0
Brendan Lestander <blestander@gmail.com>
2020-05-25 15:56:26 -0400

    Created score table for SenaryScoreChoiceComponent

--- src/app/senary-score-choice/senary-score-choice.component.ts
+++ src/app/senary-score-choice/senary-score-choice.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormArray, Validators, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-senary-score-choice',
     templateUrl: './senary-score-choice.component.html',
     styleUrls: ['./senary-score-choice.component.css']
 })
-export class SenaryScoreChoiceComponent implements OnInit {
+export class SenaryScoreChoiceComponent implements OnChanges {
 
     @Input() options: string[];
     @Output() vote = new EventEmitter();
 
-    constructor() { }
+    results: FormGroup;
 
-    ngOnInit(): void {
+    constructor(private fb: FormBuilder) { }
+
+    ngOnChanges(): void {
+        let controlArray = [];
+        for (let i = 0; i < this.options.length; i++)
+            controlArray.push(this.fb.control('', Validators.required));
+        this.results = this.fb.group({
+            resultsArray: this.fb.array(controlArray)
+        });
+    }
+
+    get scores(): number[] {
+        return [0, 1, 2, 3, 4, 5];
     }
 
 }
