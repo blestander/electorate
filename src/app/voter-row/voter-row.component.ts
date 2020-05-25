a2fd80949ea437248fb24998abbfe5ac0ae5863c
Brendan Lestander <blestander@gmail.com>
2020-05-25 10:52:44 -0400

    Integrated VoterRowComponent.

--- src/app/voter-row/voter-row.component.ts
+++ src/app/voter-row/voter-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-voter-row',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoterRowComponent implements OnInit {
 
+    @Input() voter;
+
     constructor() { }
 
     ngOnInit(): void {
+        console.log(this.voter);
     }
 
 }
