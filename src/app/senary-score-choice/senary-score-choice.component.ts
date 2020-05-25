ce51b22b9fcf037c7d33a91412505c65a3b4928a
Brendan Lestander <blestander@gmail.com>
2020-05-25 15:41:06 -0400

    Incorporated SenaryScoreChoiceComponent.

--- src/app/senary-score-choice/senary-score-choice.component.ts
+++ src/app/senary-score-choice/senary-score-choice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-senary-score-choice',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./senary-score-choice.component.css']
 })
 export class SenaryScoreChoiceComponent implements OnInit {
+
+    @Input() options: string[];
+    @Output() vote = new EventEmitter();
 
     constructor() { }
 
