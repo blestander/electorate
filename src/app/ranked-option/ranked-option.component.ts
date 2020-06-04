06f777f9d14c93da538a4433d8899964772094e3
Brendan Lestander <blestander@gmail.com>
2020-06-04 07:06:57 -0400

    Ranked options now live in their own component.

--- src/app/ranked-option/ranked-option.component.ts
+++ src/app/ranked-option/ranked-option.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-ranked-option',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./ranked-option.component.css']
 })
 export class RankedOptionComponent implements OnInit {
+
+    @Input() option: string;
 
     constructor() { }
 
