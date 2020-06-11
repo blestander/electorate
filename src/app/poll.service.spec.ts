0b232340f6ec958698862bf65e97192498c7982e
Brendan Lestander <blestander@gmail.com>
2020-06-11 13:11:53 -0400

    Created inital test and setup for PollService.

--- src/app/poll.service.spec.ts
+++ src/app/poll.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PollService } from './poll.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PollService', () => {
     let service: PollService;
+    let httpClient: HttpClient;
+    let httpController: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
+            providers: [HttpClient],
+        });
+
+        httpClient = TestBed.inject(HttpClient);
+        httpController = TestBed.inject(HttpTestingController);
         service = TestBed.inject(PollService);
     });
 
     it('should be created', () => {
-        expect(service).toBeTruthy();
+        expect(service).toBeDefined();
     });
 });
