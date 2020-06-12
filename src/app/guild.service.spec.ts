922a372ab749538914438024fbdd8e391b39389f
Brendan Lestander <blestander@gmail.com>
2020-06-12 11:20:42 -0400

    Tested getGuilds() success case.

--- src/app/guild.service.spec.ts
+++ src/app/guild.service.spec.ts
@@ -29,4 +29,33 @@ describe('GuildService', () => {
     it('should be created', () => {
         expect(service).toBeDefined();
     });
+
+    it("#getGuilds succeeds successfully", () => {
+        // Send request
+        service.getGuilds().subscribe({
+            next: guilds => {
+                expect(guilds.length).toBe(2);
+                expect(guilds[0].name).toBe("Guild 1");
+                expect(guilds[1].name).toBe("Guild 2");
+            },
+            error: error => fail(`Expected array of guilds; got error code ${error.status}`)
+        });
+
+        // Expecting request to correct URL
+        const request = httpController.expectOne("http://localhost:8080/api/guilds");
+
+        // Expecting request to be GET request
+        expect(request.request.method).toBe("GET");
+
+        // Respond to request
+        request.flush([
+            { name: "Guild 1" },
+            { name: "Guild 2" },
+        ]);
+    });
+
+    afterEach(() => {
+        // Verify that there are no outstanding requests
+        httpController.verify();
+    })
 });
