014d6b46ed51e5978f3c67094510e1beb77d95a8
Brendan Lestander <blestander@gmail.com>
2020-06-12 11:10:34 -0400

    Setup GuildService testing.

--- src/app/guild.service.spec.ts
+++ src/app/guild.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GuildService } from './guild.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { AuthService } from './auth.service';
 
-xdescribe('GuildService', () => {
+describe('GuildService', () => {
     let service: GuildService;
+    let authService: AuthService;
+    let httpController: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        // Create AuthService spy
+        authService = jasmine.createSpyObj('AuthService', ['reportLoginStatus']);
+
+        // Configure test bed
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                { provides: AuthService, useValue: authService}
+            ],
+        });
+
+        // Get objects we need to test
+        httpController = TestBed.inject(HttpTestingController);
         service = TestBed.inject(GuildService);
     });
 
     it('should be created', () => {
-        expect(service).toBeTruthy();
+        expect(service).toBeDefined();
     });
 });
