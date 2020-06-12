47f338cf65a3606b0eeb61ad38b86476e61630d1
Brendan Lestander <blestander@gmail.com>
2020-06-12 13:40:01 -0400

    Setup for testing AuthService.

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-xdescribe('AuthService', () => {
+describe('AuthService', () => {
     let service: AuthService;
+    let router: Router;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        // Create router spy
+        router = jasmine.createSpyObj(
+            'Router',
+            ['navigateByUrl'],
+            { url: 'somewhere' }
+        );
+
+        // Configure test bed
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                { provide: Router, useValue: router }
+            ]
+        });
+
+        // Get objects we need to test
         service = TestBed.inject(AuthService);
+
+        // Clear out sessionStorage
+        sessionStorage.clear();
     });
 
     it('should be created', () => {
-        expect(service).toBeTruthy();
+        expect(service).toBeDefined();
     });
 });
