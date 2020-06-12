a3fcd74bc75f5b9271ab9ca4e25a7b21f3d92879
Brendan Lestander <blestander@gmail.com>
2020-06-12 14:13:10 -0400

    Tested AuthService.retrieveToken success case.

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -2,12 +2,13 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DISCORD_SCOPE, DISCORD_CLIENT_ID } from './constants';
 
 describe('AuthService', () => {
     let service: AuthService;
     let router: Router;
+    let httpController: HttpTestingController;
 
     beforeEach(() => {
         // Create router spy
@@ -26,6 +27,7 @@ describe('AuthService', () => {
         });
 
         // Get objects we need to test
+        httpController = TestBed.inject(HttpTestingController);
         service = TestBed.inject(AuthService);
 
         // Clear out sessionStorage
@@ -91,5 +93,32 @@ describe('AuthService', () => {
         // Expect redirect URL to be correct
         const correctURL = `https://discordapp.com/api/oauth2/authorize?response_type=code&scope=${DISCORD_SCOPE}&client_id=${DISCORD_CLIENT_ID}&redirect_uri=origin/auth&state=${state}&prompt=none`;
         expect(service['externalRedirect']).toHaveBeenCalledWith(correctURL);
+    });
+
+    it("#retrieveToken completes a started authorization correctly", () => {
+        // Setup sessionStorage state
+        sessionStorage.setItem('state-alpha', JSON.stringify({
+            redirect: "/somewhere",
+            remember: false
+        }));
+
+        // Call function
+        service.retrieveToken('code-alpha', 'state-alpha');
+
+        // Expecting a call to correct URL
+        const request = httpController.expectOne("http://localhost:8080/api/login");
+
+        // Expecting request to be a POST request
+        expect(request.request.method).toBe("POST");
+
+        // Expecting request body to contain correct elements
+        expect(request.request.body.code).toBe('code-alpha');
+        expect(request.request.body.remember).toBeFalse();
+
+        // Responding to request
+        request.flush(null);
+
+        // Expecting call to router
+        expect(router.navigateByUrl).toHaveBeenCalledWith('/somewhere');
     });
 });
